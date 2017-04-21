9b610dce09d01f473f194582f540767de2c68749
Xin Li <aehrt55@aehrt55.net>
2017-04-21 14:41:57 +0800

    remove white spaces

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -82,7 +82,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -128,7 +128,7 @@ module.exports = {
         include: [paths.appSrc, paths.appModules],
         loader: 'babel',
         query: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
@@ -162,7 +162,7 @@ module.exports = {
       // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -86,7 +86,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -125,7 +125,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: [paths.appSrc, paths.appModules],
         loader: 'babel',
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -166,7 +166,7 @@ module.exports = {
       // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
