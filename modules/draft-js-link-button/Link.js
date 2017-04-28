2b0643452ba15610cb7da23c3c5cf6031c78ac75
Xin Li <aehrt55@aehrt55.net>
2017-04-28 16:38:24 +0800

    add style to Link

--- modules/draft-js-link-button/Link.js
+++ modules/draft-js-link-button/Link.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Link = ({ contentState, entityKey, children }) => (
+const Link = ({ contentState, entityKey, children, style }) => (
   <a
     href={contentState.getEntity(entityKey).getData().url}
-    style={{ textDecoration: 'none' }}
+    style={{ textDecoration: 'none', ...style }}
   >
     {children}
   </a>
@@ -13,6 +13,10 @@ Link.propTypes = {
   contentState: PropTypes.object.isRequired,
   entityKey: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  style: PropTypes.object.isRequired,
+};
+Link.defaultProps = {
+  style: {},
 };
 
 export default Link;
