28ea47617a91fc0ae7804d73a8dfe85d9d450ef8
Xin Li <aehrt55@aehrt55.net>
2017-04-28 11:26:45 +0800

    change decorator style

--- modules/draft-js-link-button/Link.js
+++ modules/draft-js-link-button/Link.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Link = ({ contentState, entityKey, children, getEditorState }) => (
+const Link = ({ contentState, entityKey, children }) => (
   <a
     href={contentState.getEntity(entityKey).getData().url}
     style={{
-      backgroundColor: getEditorState().getSelection().getHasFocus() ? 'rgba(255,200,0, .4)' : 'transparent',
+      borderBottom: '1px dotted #666',
       textDecoration: 'none',
     }}
   >
@@ -16,7 +16,6 @@ Link.propTypes = {
   contentState: PropTypes.object.isRequired,
   entityKey: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  getEditorState: PropTypes.func.isRequired,
 };
 
 export default Link;
