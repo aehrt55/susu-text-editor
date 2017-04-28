bda7eed8c9ad60a43329ef429a9bb8d81ba5249f
Xin Li <aehrt55@aehrt55.net>
2017-04-28 14:13:18 +0800

    modify Link style

--- modules/draft-js-link-button/Link.js
+++ modules/draft-js-link-button/Link.js
@@ -4,10 +4,7 @@ import PropTypes from 'prop-types';
 const Link = ({ contentState, entityKey, children }) => (
   <a
     href={contentState.getEntity(entityKey).getData().url}
-    style={{
-      borderBottom: '1px dotted #666',
-      textDecoration: 'none',
-    }}
+    style={{ textDecoration: 'none' }}
   >
     {children}
   </a>
