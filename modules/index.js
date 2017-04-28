8310cf339d8f280c256ecce68badc405a7756592
Xin Li <aehrt55@aehrt55.net>
2017-04-28 12:08:24 +0800

    export modules

--- modules/index.js
+++ modules/index.js
@@ -1,1 +1,7 @@
 export default from './susu-text-editor';
+export Blocks from './draft-js-blocks';
+export Buttons from './draft-js-buttons';
+export ColorButton from './draft-js-color-button';
+export FontSizeButton from './draft-js-font-size-button';
+export InlineToolbarPlugin from './draft-js-inline-toolbar-plugin';
+export LinkButton from './draft-js-link-button';
